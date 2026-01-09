cd8d3372a934ae53bfd462286a08d0ec2833b199
RoDol84 <ronnyd@live.de>
2026-01-09 11:54:54 +0000

    Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -14,10 +14,12 @@
 
 /* Deklaration + Wertzuweisung I */
 
-
-
-
-
+let firstName; // Deklaration (Difinition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung Assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("HI, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
 
 
 /*  
@@ -32,4 +34,5 @@
 
 
 /* Variablen vs. Konstanten in JS */
+
 
