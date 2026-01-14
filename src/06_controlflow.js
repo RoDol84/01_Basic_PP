cbb8b96aee165815d3e2e1688538873c5a744dfa
Peter Schmidt <schmidt.multimedia@gmail.com>
2026-01-14 10:24:30 +0000

    06 / Fallunterscheidung / SWITCH|CASE 1

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -65,3 +65,39 @@ console.log("------------------");
 
 /************  Ternäre (ternary) Schreibweise ************/
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";      // .. fährt TAXI! / UBER
+job = "diver";       // .. taucht im Rhein!
+job = "artist";      // .. malt ein Bild!
+job = "pilot";       // .. macht etwas anderes! --> default
+job = "teacher";     // .. unterrichtet!
+job = "instructor";  // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver":
+        console.log(firstName + " fährt TAXI!");
+        break;
+
+    case "diver":
+        console.log(firstName + " taucht im Rhein!");
+        break;
+
+    case "artist":
+        console.log(firstName + " malt ein Bild!");
+        break;
+
+    case "teacher":
+    case "instructor":
+        console.log(firstName + " unterrichtet!");
+        break;
+
+    default: // WICHTIG !!!!
+        console.log(firstName + " macht etwas anderes!");
+        break;
+}
